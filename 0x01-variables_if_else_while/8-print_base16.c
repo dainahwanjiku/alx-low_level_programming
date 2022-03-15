03f9aeed082f07d92d86ce76bdd4fdc3b4732a54
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-03-15 05:07:06 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,6 +9,7 @@ int main(void)
 	int num;
 	/* the black line belongs over here */
 	char letter;
+	/* the blank line belongs here too */
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 	for (letter = 'a'; letter <= 'f'; letter++)
