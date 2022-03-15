ffdd630ca1e7f2a93aba6d63ea961ed1bdcc7c72
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-03-15 03:04:01 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main -entry point
  *
- * return: always 0
+ * return: Always 0
  */
 int main(void)
+  
 {
 puts("\"Programming is like building a multilingual puzzle");
+  
 return (0);
 }
