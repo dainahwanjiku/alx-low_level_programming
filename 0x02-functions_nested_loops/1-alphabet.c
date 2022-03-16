a899999a4174417735ad10b30551c42b5e15a085
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-03-16 04:04:55 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,10 +9,12 @@
  */
 void print_alphabet(void)
 {
-	char letter;
+	char letter; char c;
 	/* blank line */
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++) 
+	{
 		_putchar(letter);
-	_putchar('\n');
+	}
+	_putchar('\n'); 
 }
 
