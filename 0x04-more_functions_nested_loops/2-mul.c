33444fb258542aacba66b558fd3fbab4b1461767
dainahwanjiku <deeshish2017@gmail.com>
2022-03-17 17:15:54 -0700

    morefunctionsnestedloops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -function that multiples two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
