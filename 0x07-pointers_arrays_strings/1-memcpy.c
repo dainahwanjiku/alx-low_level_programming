e164aaa634ba6642a8baeb95e4e322f5cc585749
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-03-29 05:14:09 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,10 @@
  char *_memcpy(char *dest, char *src, unsigned int n)
  {
          unsigned int i;
-         /* blank line */
+          /* blank line */
          for (i = 0; i < n; i++)
          {
-      
-                 *(dest + i) = *(src + i);
+                  *(dest + i) = *(src + i);
          }
          return (dest);
  }                                               
