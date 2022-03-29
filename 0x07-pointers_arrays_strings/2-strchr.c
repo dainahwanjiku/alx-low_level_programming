4ce473680ec3c15719e25e4f1fd082edce883dc9
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-03-29 05:18:14 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  *_strchr - locates a character in a string
  *@s: string to check
  *@c: character to check for
- *
- * Return: pointer to spot in s with c or null
+ *Return: pointer to spot in s with c or null
  */
  char *_strchr(char *s, char c)
 {
