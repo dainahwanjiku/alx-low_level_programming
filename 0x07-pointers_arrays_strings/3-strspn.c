10aca41b4f7480abd5d093372ddb98e534306674
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-03-29 05:32:58 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strspn - gets lengthof a prefix substring
- * @s: string to check
- * @accept: string to check against
+ *_strspn - gets lengthof a prefix substring
+ *@s: string to check
+ *@accept: string to check against
  *
- * Return: number of bytes of s in accept
+ *Return: number of bytes of s in accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
