2fe390a85a11985d6ce6cf544cc1c8f357175591
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-04-02 04:22:48 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,6 @@
  *
  * Return: 0 if no errors, else 1
  */
-
 int main(int argc, char *argv[])
 {
 	int a, b, c;
@@ -16,7 +15,8 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		puts("Error")
-		return (1);								}
+		return (1);
+	}
 	a = atoi(argv[1]);
         b = atoi(argv[2]);
         c = a * b;
