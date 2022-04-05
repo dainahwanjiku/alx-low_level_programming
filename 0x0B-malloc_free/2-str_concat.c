777e2038607ed36bd24b7491af317ad64b03bf15
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-04-05 04:07:43 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,14 +17,14 @@ char *str_concat(char *s1, char *s2)
 		i = 0;
 	else
 	{
-		for (i =0; s1[i]; i++)
+		for (i = 0; s1[i]; i++)
 			;
 	}
 	if (s2 == NULL)
 		j = 0;
 	else
 	{
-		for (j =0; s2[j]; j++)
+		for (j = 0; s2[j]; j++)
 			;
 	}
 	k = i + j + 1;
