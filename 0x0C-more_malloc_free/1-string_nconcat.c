25ebde6aad9527cdebc817deb2d5dba11353c234
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-04-08 04:03:26 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,29 +18,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
         /* blank line */
 	if (s1 == NULL)
 		s1 = "";
-	
+	/* blank line */
 	if (s2 == NULL)
 		s2 = "";
-	
+	/* blank line */
 	for (index = 0; s1[index]; index++)
 		len++;
-	
+	/* blank line */
 	concat = malloc(sizeof(char) * (len + 1));
-	
+	/* blank line */
 	if (concat == NULL)
 		return (NULL);
-	
+	/* blank line */
 	len = 0;
-	
+	/* blank line */
 	for (index = 0; s1[index]; index++)
-		
+	/* blank line */
 		concat[len++] = s1[index];
-	
+	/* blank line */
 	for (index = 0; s2[index] && index < n; index++)
-		
+		/* blank line*/
 		concat[len++] = s2[index];
-	
+	/* blank line */
 	concat[len] = '\0';
-	
+	/* blank line */
 	return (concat);
 }
