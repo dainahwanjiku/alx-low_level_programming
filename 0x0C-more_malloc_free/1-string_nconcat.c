629ba2f2e2388cbcd940775369fdf8a63ff3ed51
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-04-08 05:06:25 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,34 +13,42 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
-        /* blank line */
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
+
 	if (s1 == NULL)
 		s1 = "";
-	
+
 	if (s2 == NULL)
 		s2 = "";
-	
-	for (index = 0; s1[index]; index++)
-		len++;
-	
-	concat = malloc(sizeof(char) * (len + 1));
-	
-	if (concat == NULL)
-		return (NULL);
-	
-	len = 0;
-	
-	for (index = 0; s1[index]; index++)
-	
-		concat[len++] = s1[index];
-	
-	for (index = 0; s2[index] && index < n; index++)
-		
-		concat[len++] = s2[index];
-	
-	concat[len] = '\0';
-	
-	return (concat);
+
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
