0894ae3a7743f83c38a438ca648194bb1e12ee4b
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-04-11 20:12:23 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,13 +9,12 @@
  *
  * Return: void
  */
- void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
       if (d)
-	    {
-             d->name = name;
-		         d->age = age;
-		         d->owner = owner;
+      {
+              d->name = name;
+	      d->age = age;
+	      d->owner = owner;
       }
 }
-     
