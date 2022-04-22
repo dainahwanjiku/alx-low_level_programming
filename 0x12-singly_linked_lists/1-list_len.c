1f03a501ffedcf54649909631fee53bbdc460e41
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-04-22 04:42:41 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,9 @@
 
 /**
  * list_len - function that returns the number of elements in a linked list_t list.
- * @h - pointer to the start of the linked list 
+ * @h: pointer to the start of the linked list 
  *
  * Return: the number of elements in a linked list_t list
- *
  */
 size_t list_len(const list_t *h)
 {
