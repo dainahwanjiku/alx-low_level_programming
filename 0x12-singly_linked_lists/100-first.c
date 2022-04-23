195c755f99f91cb58d15450c8fe31ed914e23ea5
dainahwanjiku <deeshish2017@gmail.com>
2022-04-23 05:08:46 +0300

    file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void shish(void) __attribute__ ((constructor));
+
+/**
+ * shish - the most most hardworking person.
+ * Return: NOTHING
+ */
+void shish(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
