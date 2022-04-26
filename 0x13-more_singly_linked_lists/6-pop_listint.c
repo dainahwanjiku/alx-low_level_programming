081be713dead9042914c67588d33965b8a01c22b
dainahwanjiku <deeshish2017@gmail.com>
2022-04-26 04:56:10 +0300

    edits

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,17 +3,19 @@
 /**
  * pop_listint - deletes the head node of a listint_t linked list.
  * if the linked list is empty return 0
- * head: 
+ * head: the head node.
  *
  * Return: the head node’s data (n).
  */
 int pop_listint(listint_t **head)
 {
-
-
-
-printf("    \n", );
-
-
-
+	listint_t *tmp;
+	int value = 0;
+	if ( *head == NULL)
+		return (0);
+	tmp = (*head)->next;
+	value = (*head)->n;
+	free(*head);
+	*head = tmp;
+	return (value);
 }
