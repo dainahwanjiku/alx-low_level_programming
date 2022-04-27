6043a0d891e5a6970b34f8a85aa9874556a549dd
dainahwanjiku <deeshish2017@gmail.com>
2022-04-27 06:08:16 +0300

    singlylinkedlists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,16 +1,38 @@
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position.
- * @n
- * @idx: is the index of the list where the new node should be added. Index starts at 0
- * if it is not possible to add the new node at index idx, do not add the new node and return  * NULL
- *
- * Returns: the address of the new node, or NULL if it failed
+ * insert_nodeint_at_index - insert a node at some index
+ * @head: The head node
+ * @idx: index given by main
+ * @n: number to insert in the node
+ * Return: @new  address of the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	printf("      \n", );
+	listint_t *new;
+	listint_t *tmp;
+	unsigned int position = 1;/*It stars counting positions at 1*/
 
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	if (*head == 0)
+		return (NULL);
+	tmp = *head;
+	while (position < idx)
+	{
+		if (tmp == NULL)
+			return (NULL);
+		tmp = tmp->next;/*to put tmp in the place we want*/
+		position++;
+	}
+	new->next = tmp->next;
+	tmp->next = new;
+	return (new);
 }
-
