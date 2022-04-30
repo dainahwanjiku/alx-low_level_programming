d8972497da97c24fb26238f31d716c8f808605b3
dainahwanjiku <deeshish2017@gmail.com>
2022-04-30 04:20:10 +0300

    bit files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,35 @@
 #include "main.h"
 
 /**
+ * _strlen - finding the length of a string.
+ * @s: first pointer
+ * Return: it returns the length of the string.
+ */
+int _strlen(char *s)
+{
+	int str;
+
+	str = 0;
+	while (s[str] != '\0')
+		str++;
+	return (str);
+}
+/**
+ * x_to_the_n - Fucntion that does to the power of
+ * @x: the number
+ * @n: the eleveado
+ * Return:@number the result
+ */
+int x_to_the_n(int x, int n)
+{
+	int i;
+	int number = 1;
+
+	for (i = 0; i < n; ++i)
+		number *= x;
+	return (number);
+}
+/**
  * binary_to_uint - function that converts a binary number to an unsigned int.
  * @b is pointing to a string of 0 and 1 chars.
  *
@@ -9,23 +38,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int j = 0, operand = 0, len = 0;
+	unsigned int result = 0;
 
-	int i;
-	unsigned int num;
-
-	num = 0;
-	if(!b) 
-        	return (NULL);
-	for (i = 0; b[i] != '\0'; i++)
+	if (!b)
+		return (0);
+	len = _strlen((char *) b);
+	while (b[j] != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[j] < 48 || b[j] > 49)
 			return (0);
+		operand = x_to_the_n(2, j);
+		if (b[len - 1] == 49)/*Ascci code of 1*/
+			result += operand;
+		len--;
+		j++;
 	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
-	}
-	return (num);
+	return (result);
 }
