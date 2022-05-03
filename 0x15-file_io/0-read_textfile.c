b137159c6d83e8432315883a22f00db6289d3705
dainahwanjiku <deeshish2017@gmail.com>
2022-05-03 20:11:49 +0300

    file1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,32 +9,35 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int w, file, result;
+	char *buf;
 
-	int fd;
-	ssize_t lenr, lenw;
-	char *buffer;
-
-	if (filename == NULL)
+	buf = malloc(sizeof(char) * letters + 1);
+	if (!buf || !filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		close(fd);
+		close(file);
+		free(buf);
 		return (0);
 	}
-	lenr = read(fd, buffer, letters);
-	close(fd);
-	if (lenr == -1)
+	result = read(file, buf, letters);
+	if (result == -1)
 	{
-		free(buffer);
+		close(file);
+		free(buf);
 		return (0);
 	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-	if (lenr != lenw)
+	buf[result] = '\0';/*Null char at the end*/
+	w = write(STDOUT_FILENO, buf, result);/*printf not allowed*/
+	if (w == -1)
+	{
+		close(file);
+		free(buf);
 		return (0);
-	return (lenw);
+	}
+	close(file);
+	free(buf);
+	return (result);
 }
