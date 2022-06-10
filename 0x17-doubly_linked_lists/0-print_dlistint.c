547148c487b7185b55ab00f2c8d426a9071a1eee
dainahwanjiku <deeshish2017@gmail.com>
2022-06-10 17:50:46 +0300

    doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,18 +7,16 @@
  *
  * Return: number of nodes
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nbr_nodes = 0;
 	const dlistint_t *current = h;
-
+        /* blank line */
 	while (current != NULL)
 	{
 		printf("%i\n", current->n);
 		current = current->next;
 		nbr_nodes++;
 	}
-
 	return (nbr_nodes);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,6 +2,9 @@
 #define LISTS_H
 
 
+#include <stdlib.h>
+
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -27,7 +30,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
 
 
 #endif LISTS_H
