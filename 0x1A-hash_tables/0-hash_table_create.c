bd7a0e266d7fa64b96d96e55d082a261481b52cb
dainahwanjiku <deeshish2017@gmail.com>
2022-07-01 16:20:28 +0300

    file 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,9 +14,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	// creates a new hashtable
 	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL){
+	if (hash_table == NULL)
 		return (NULL);
-	}
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if(hash_table->array == NULL)
