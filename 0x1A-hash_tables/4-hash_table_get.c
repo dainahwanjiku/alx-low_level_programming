46f5d1550b8f28e7ae4e25afdc6aa3e314452702
dainahwanjiku <deeshish2017@gmail.com>
2022-07-02 00:49:33 +0300

    file 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp;
 
-	ht* hash_table = hash_table->array[index];
+	if (ht == NULL || ht->array == NULL || ht->size == 0 || 
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
 	//ensure that we move to a non null array
 	if (tmp != NULL)
 	{
